11b93ee0524f93fb77f69bd4ae8d6faf6a52f548
Allan M Alegretti <allanalegretti@gmail.com>
2023-03-01 16:50:19 -0300

    testando a manipulacao da DOM

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+one.onclick = function () { answer.innerHTML = '1'; }
+two.onclick = function () { display("2"); }
+three.onclick = function () { plus(2, 4); }
+four.onclick = function () { erro() }
+document.getElementById("five").addEventListener("click", function () { display(5) });
+document.getElementById("six").addEventListener("click", function () { display(6) });
+
+function display(i) {
+    answer.innerHTML = i;
+}
+
+
+
 clear.onclick = function () {
     answer.innerHTML = '';
+}
+
+clear2.onclick = function () {
+    answer.innerHTML = '';
+}
+
+function plus (a, b){
+    answer.innerHTML = a+b;
+}
+
+function erro (){
+    answer.innerHTML = 'ERRO';
 }
